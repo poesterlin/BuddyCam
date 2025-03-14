5a63bdd9238f3ad739931c0b4ef6caf03af0ed3c
Philip Oesterlin <philip.oesterlin@gmail.com>
2025-03-14 23:54:27 +0100

    fallback match making

--- src/routes/(authenticated)/+page.server.ts
+++ src/routes/(authenticated)/+page.server.ts
@@ -1,7 +1,7 @@
 import { db } from '$lib/server/db';
 import { eventsTable, friendsTable, matchupTable } from '$lib/server/db/schema';
 import { generateId, validateAuth } from '$lib/server/util';
-import { and, eq } from 'drizzle-orm';
+import { and, eq, gt, inArray, isNull } from 'drizzle-orm';
 import type { Actions, PageServerLoad } from './$types';
 import { EventType, type ReadyRequestData } from '$lib/events';
 import { redirect } from '@sveltejs/kit';
@@ -28,6 +28,34 @@ export const actions: Actions = {
 			redirect(302, '/friends');
 		}
 
+		// if a friend started a match in the last 5 minutes, join them
+		const cutoff = new Date();
+		cutoff.setMinutes(cutoff.getMinutes() - 5);
+
+		const [recentMatch] = await db
+			.select({
+				id: matchupTable.id,
+				userId: matchupTable.userId,
+				friendId: matchupTable.friendId
+			})
+			.from(matchupTable)
+			.where(
+				and(
+					inArray(
+						matchupTable.userId,
+						friends.map((friend) => friend.id)
+					),
+					isNull(matchupTable.friendId),
+					gt(matchupTable.createdAt, cutoff)
+				)
+			)
+			.orderBy(matchupTable.createdAt)
+			.limit(1);
+
+		if (recentMatch) {
+			redirect(302, '/cam/waiting-room/' + recentMatch.id);
+		}
+
 		// create a new matchup
 		const matchId = generateId();
 		await db.insert(matchupTable).values({
@@ -50,7 +78,7 @@ export const actions: Actions = {
 					matchId
 				} satisfies ReadyRequestData,
 				isTechnical: false,
-				persistent: true,
+				persistent: true
 			} satisfies typeof eventsTable.$inferInsert;
 		});
 
@@ -59,4 +87,3 @@ export const actions: Actions = {
 		redirect(302, '/cam/waiting-room/' + matchId);
 	}
 };
-		
