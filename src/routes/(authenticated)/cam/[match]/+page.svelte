411bb02bcf6e1cd7f045b9a10e78e212afb2ae8e
Philip Oesterlin <philip.oesterlin@gmail.com>
2025-03-31 15:04:41 +0200

    only have the owner send the webrtc request

--- src/routes/(authenticated)/cam/[match]/+page.svelte
+++ src/routes/(authenticated)/cam/[match]/+page.svelte
@@ -66,10 +66,10 @@
 			console.log('Client time is ahead of server time by', timeDiff, 'ms');
 		}
 
-		// if (data.matchup.userId === data.user.id) {
-		console.log('Creating WebRTC offer');
-		createWebRtcOffer();
-		// }
+		if (data.matchup.userId === data.user.id) {
+			console.log('Creating WebRTC offer');
+			createWebRtcOffer();
+		}
 	});
 
 	onDestroy(() => {
