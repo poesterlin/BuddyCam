8830c4cd407e850232f3b66a36d7ffba5f2f2dc2
Philip Oesterlin <philip.oesterlin@gmail.com>
2025-03-31 12:16:19 +0200

    debug parsing

--- src/routes/(authenticated)/cam/[match]/webrtc/+server.ts
+++ src/routes/(authenticated)/cam/[match]/webrtc/+server.ts
@@ -69,7 +69,7 @@ export const POST: RequestHandler = async (event) => {
 		persistent: false,
 		data: {
 			matchId: match,
-			data: parsedBody.data
+			data: body
 		} satisfies WebRtcData
 	} satisfies typeof eventsTable.$inferInsert);
 
